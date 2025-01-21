d50dc52ea19dd9c146faf50ec779ba1ffff27a70
alexpinter <pinter.alex.2020i@bankitatabanya.hu>
2025-01-21 13:00:49 +0100

    2025 01 21

--- app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -256,7 +256,7 @@ class MainActivity : AppCompatActivity() {
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
-//        }fdd
+//        }fddgf
 
         //ui elemek inicializálása
         openGreetingButton = findViewById(R.id.openGreetingButton)
